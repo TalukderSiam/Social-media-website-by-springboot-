9c7447fa518d6d99504649946a694b8f0298847d
siam <siamtalukder135@gamil.com>
2024-07-05 00:52:23 +0600

    Complete register and login and security

---
+++ src/main/java/com/zosh/ZoshSocialYoutube222Application.java
@@ -0,0 +1,13 @@
+package com.zosh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoshSocialYoutube222Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoshSocialYoutube222Application.class, args);
+	}
+
+}
